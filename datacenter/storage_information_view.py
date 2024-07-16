807db6126a5f9d83ff4797abd35c0ca57cdffe66
Stmkv <stepkamakarov1999@mail.ru>
2024-07-16 12:26:50 +0300

    Отформатировал код

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,19 +1,28 @@
-from datacenter.models import Passcard
+from time import localtime
+from django.utils.timezone import localtime  # noqa: F811
 from datacenter.models import Visit
 from django.shortcuts import render
+from .models import get_duration, format_duration
+import locale
+
+locale.setlocale(locale.LC_ALL, "ru_RU.UTF-8")
 
 
 def storage_information_view(request):
-    # Программируем здесь
+    visits_with_leaved_at = Visit.objects.filter(leaved_at__isnull=True)
 
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
+    non_closed_visits = []
+    for visit in visits_with_leaved_at:
+        duration = get_duration(visit)
+        duration_str = format_duration(duration)
+        non_closed_visit = {
+            "who_entered": visit.passcard,
+            "entered_at": localtime(visit.entered_at).strftime("%d %B %Y г. %H:%M"),
+            "duration": duration_str,
         }
-    ]
+        non_closed_visits.append(non_closed_visit)
+
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        "non_closed_visits": non_closed_visits,
     }
-    return render(request, 'storage_information.html', context)
+    return render(request, "storage_information.html", context)
