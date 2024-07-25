ad6cd73e6db62cc53eedbf24c1e82300afba8c7b
Stmkv <stepkamakarov1999@mail.ru>
2024-07-25 15:21:31 +0300

    Скрыл необходимые данные

--- project/settings.py
+++ project/settings.py
@@ -1,43 +1,49 @@
 import os
+from dotenv import load_dotenv
+from environs import Env
+
+env = Env()
+env.read_env()
+
+load_dotenv()
+
+DATABASE_ENGINE = env("DATABASE_ENGINE")
+DATABASE_HOST = env("DATABASE_HOST")
+DATABASE_PORT = env("DATABASE_PORT")
+DATABASE_NAME = env("DATABASE_NAME")
+DATABASE_USER = env("DATABASE_USER")
+DATABASE_PASSWORD = env("DATABASE_PASSWORD")
+SECRET_KEY = env("SECRET_KEY")
+ROOT_URLCONF = env("ROOT_URLCONF")
+DEBUG = env.bool("DEBUG", False)
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'checkpoint.devman.org',
-        'PORT': '5434',
-        'NAME': 'checkpoint',
-        'USER': 'guard',
-        'PASSWORD': 'osim5',
+    "default": {
+        "ENGINE": DATABASE_ENGINE,
+        "HOST": DATABASE_HOST,
+        "PORT": DATABASE_PORT,
+        "NAME": DATABASE_NAME,
+        "USER": DATABASE_USER,
+        "PASSWORD": DATABASE_PASSWORD,
     }
 }
 
-INSTALLED_APPS = ['datacenter']
+INSTALLED_APPS = ["datacenter"]
 
-SECRET_KEY = 'REPLACE_ME'
-
-DEBUG = True
-
-ROOT_URLCONF = 'project.urls'
-
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ["*"]
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'APP_DIRS': True,
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
     },
 ]
 
-
 USE_L10N = True
-
-LANGUAGE_CODE = 'ru-ru'
-
-TIME_ZONE = 'Europe/Moscow'
-
+LANGUAGE_CODE = "ru-ru"
+TIME_ZONE = "Europe/Moscow"
 USE_TZ = True
-
-DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
