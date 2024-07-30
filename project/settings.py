c72f63251950808e0a6f353be490bc800303c0fb
Stmkv <stepkamakarov1999@mail.ru>
2024-07-30 16:34:12 +0300

    Высушил код

--- project/settings.py
+++ project/settings.py
@@ -1,49 +1,47 @@
 import os
-from dotenv import load_dotenv
 from environs import Env
 
 env = Env()
 env.read_env()
 
-load_dotenv()
-
-DATABASE_ENGINE = env("DATABASE_ENGINE")
-DATABASE_HOST = env("DATABASE_HOST")
-DATABASE_PORT = env("DATABASE_PORT")
-DATABASE_NAME = env("DATABASE_NAME")
-DATABASE_USER = env("DATABASE_USER")
-DATABASE_PASSWORD = env("DATABASE_PASSWORD")
-SECRET_KEY = env("SECRET_KEY")
-ROOT_URLCONF = env("ROOT_URLCONF")
-DEBUG = env.bool("DEBUG", False)
 
 DATABASES = {
-    "default": {
-        "ENGINE": DATABASE_ENGINE,
-        "HOST": DATABASE_HOST,
-        "PORT": DATABASE_PORT,
-        "NAME": DATABASE_NAME,
-        "USER": DATABASE_USER,
-        "PASSWORD": DATABASE_PASSWORD,
+    'default': {
+        'ENGINE': env.str('DB_ENGINE'),
+        'HOST': env.str('DB_HOST'),
+        'PORT': env.int('DB_PORT'),
+        'NAME': env.str('DB_NAME'),
+        'USER': env.str('DB_USER'),
+        'PASSWORD': env.str('DB_PASSWORD'),
     }
 }
 
-INSTALLED_APPS = ["datacenter"]
+DEBUG = env.bool('DEBUG', False)
 
-ALLOWED_HOSTS = ["*"]
+INSTALLED_APPS = ['datacenter']
+
+SECRET_KEY = env.str('SECRET_KEY')
+
+ROOT_URLCONF = 'project.urls'
+
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "templates")],
-        "APP_DIRS": True,
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
     },
 ]
 
 USE_L10N = True
-LANGUAGE_CODE = "ru-ru"
-TIME_ZONE = "Europe/Moscow"
+
+LANGUAGE_CODE = 'ru-ru'
+
+TIME_ZONE = 'Europe/Moscow'
+
 USE_TZ = True
-DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
